84d2cdc698bc505629d03a1c1d2d1a45cd00eacc
miorajulia01 <miorahanitra01@gmail.com>
2026-06-03 09:33:05 +0300

    fix: resolve app import in App.spec

--- employes-admin/src/App.tsx
+++ employes-admin/src/App.tsx
@@ -3,7 +3,7 @@ import jsonServerProvider from "ra-data-json-server";
 import { EmployeeList } from "./employees/EmployeeList";
 import { EmployeeCreate } from "./employees/EmployeeCreate";
 import { EmployeeEdit } from "./employees/EmployeeEdit";
-
+import { EmployeeShow } from "./employees/EmployeeShow";
 
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
@@ -14,7 +14,7 @@ const App = () => (
       list={EmployeeList}
       create={EmployeeCreate}
       edit={EmployeeEdit}
-
+      show={EmployeeShow}
     />
   </Admin>
 );
